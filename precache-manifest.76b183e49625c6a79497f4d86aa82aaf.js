087758b1e2556258b5ee19270aa6fa6c2d4885d1
Lisnyk-M <swapbuffers@gmail.com>
2020-10-17 13:15:45 +0300

    Updates

---
+++ precache-manifest.76b183e49625c6a79497f4d86aa82aaf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e4758ba5e5a25efaa64d335ba693c71",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "53806b955cca2cd6dc81",
+    "url": "/goit-react-hw-01-components/static/css/main.f822b65d.chunk.css"
+  },
+  {
+    "revision": "601f942fc7e2de3ed56b",
+    "url": "/goit-react-hw-01-components/static/js/2.c05bd6ec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.c05bd6ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53806b955cca2cd6dc81",
+    "url": "/goit-react-hw-01-components/static/js/main.e5faee67.chunk.js"
+  },
+  {
+    "revision": "1772fa44edc09e183bc9",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.d76a0c71.js"
+  }
+]);
